d5e8aa6596f7ef6bb6572298735397f85466ade1
Daniel Amaral <danielamaral652@gmail.com>
2022-03-23 00:22:13 -0300

    otmização

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
@@ -8,18 +7,53 @@ function ativarLink(link) {
     link.classList.add("ativo");
   }
 }
-
 links.forEach(ativarLink);
 
+
 // Ativar Items do Orçamento
-
 const parametros = new URLSearchParams(location.search);
-
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
   }
 }
-
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+
+// Galeria de bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  matchMedia('(min-width: 1000px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+galeria.forEach(eventosGaleria);
+
+
+// Animação
+if(window.SimpleAnime) {
+  new SimpleAnime();
+}
